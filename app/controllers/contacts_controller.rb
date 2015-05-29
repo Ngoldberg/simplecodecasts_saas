176a12eee578b40839866f519f15aae072efc7f3
Naomi Goldberg <Naomi.Goldberg@me.com>
2015-05-29 20:00:58 +0000

    fixed issues with email sending via sendgrid

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,15 +8,15 @@ class ContactsController < ApplicationController
     
     if @contact.save
       name = params[:contact][:name]
-      email = params[:contact][:name]
-      body = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
       
       ContactMailer.contact_email(name, email, body).deliver
       
-      flash[:success] = "Message Sent"
+      flash[:success] = "Message Sent."
       redirect_to new_contact_path
     else
-      flash[:danger] = "Error occured message has not been sent."
+      flash[:danger] = "Error occured, message has not been sent."
       redirect_to new_contact_path
     end
   end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,10 @@
 class ContactMailer < ActionMailer::Base
   default to: 'Naomi.Goldberg@me.com'
   
-  def contact_email(name,email,body)
+  def contact_email(name, email, body)
     @name = name
     @email = email
     @body = body
-    
     mail(from: email, subject: 'Contact Form Message')
   end
 end
